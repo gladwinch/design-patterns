39d8c4a94be680f89ab2a2141e16824ecb3d3dff
gladwinch <gladwinch20@gmail.com>
2023-08-16 12:55:00 +0530

    chain of responsibility pattern

--- chain_of_response/Store.js
+++ chain_of_response/Store.js
@@ -1,12 +1,22 @@
+const Storage = require('./Storage')
+
 class Store {
-    constructor(name, inventory=[]) {
+    constructor(name, inventory) {
         this.name = name
-        this.inventory = inventory
+        const floor = new Storage('store floor', inventory.floor)
+        const backroom = new Storage('store backroom', inventory.backroom)
+        const localstore = new Storage('nearby store', inventory.localStore, 1)
+        const warehouse = new Storage('warehouse', inventory.warehouse, 5)
+
+        floor.setNext(backroom)
+        backroom.setNext(localstore)
+        localstore.setNext(warehouse)
+
+        this.storage = floor
     }
 
     find(itemName) {
-        const index = this.inventory.map(item => item.name).indexOf(itemName)
-        return this.inventory[index]
+        return this.storage.find(itemName)
     }
 }
 
--- chain_of_response/index.js
+++ chain_of_response/index.js
@@ -1,9 +1,9 @@
 const Store = require('./Store')
 const inventory = require('./inventory.json')
 
-const skiShop = new Store('Steep and deep', inventory.floor)
+const skiShop = new Store('Steep and deep', inventory)
 
-const searchItem = 'ski hats'
+const searchItem = 'wax'
 const result = skiShop.find(searchItem)
 
 console.log(result)
