e8b8f90bd7b5bf6c97e166701cefe1f42a25bb56
gladwinch <gladwinch20@gmail.com>
2023-08-16 20:10:03 +0530

    facade pattern

---
+++ facade/index.js
@@ -0,0 +1,58 @@
+class ComplexVideoLibrary {
+    load(file) {
+        // Loads the video file...
+        console.log(`Loading video file: ${file}`);
+    }
+    
+    compress() {
+        // Compress the video...
+        console.log('Compressing the video...');
+    }
+
+    store(destination) {
+        // Stores the compressed video...
+        console.log(`Storing video to: ${destination}`);
+    }
+}
+
+class VideoFacade {
+    constructor() {
+        this.videoLibrary = new ComplexVideoLibrary();
+    }
+
+    processVideo(file, destination) {
+        this.videoLibrary.load(file);
+        this.videoLibrary.compress();
+        this.videoLibrary.store(destination);
+    }
+}
+
+const videoProcessor = new VideoFacade();
+
+videoProcessor.processVideo('sample.mp4', 'processed.mp4');
+
+// The Facade Pattern is a design pattern that provides a simplified interface to a more complex system, library, or framework. It's commonly used to hide the complexity of underlying systems and provide a clear and easy-to-use API for consumers.
+
+// Let's go through the Facade Pattern in JavaScript with a simple example.
+
+// Problem:
+// Suppose you have a complex video processing library with various functionalities like loading a file, compressing the video, and then storing it.
+
+// Using this library directly can be cumbersome, and you want to simplify the process of loading, compressing, and storing a video.
+
+// Facade Solution:
+// You can create a facade that wraps this complex video library and provides a single method to perform the entire process.
+
+// Advantages:
+// Simplicity: The Facade Pattern simplifies a complex system into a high-level interface that's easy to use.
+// Decoupling: Clients interact with the facade, not the complex system. If the internal workings of the system change, the client remains unaffected as long as the facade's interface remains consistent.
+// Flexibility: Facades can be changed or extended without affecting existing clients.
+// When to use:
+// When you want to provide a simpler or reduced interface to a larger body of code, such as a library or framework.
+// When you need to decouple client code from complex systems, promoting a loose coupling between systems.
+// When you want to group a set of functionalities and present a unified API.
+// However, it's worth noting that overusing the facade pattern can lead to issues, such as hiding necessary complexities, which might be crucial for certain advanced use cases. Always assess the situation and consider if the abstraction offered by a facade is truly beneficial.
+
+
+
+
